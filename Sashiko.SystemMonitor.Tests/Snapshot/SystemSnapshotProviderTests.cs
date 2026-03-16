11ac38810c157e36f799a05a05bac5538bf07388
Alexandru Luca <alex.luca98@gmail.com>
2026-03-16 19:16:50 +0100

    chore(tests): fix namespace reference in test project

--- Sashiko.SystemMonitor.Tests/Snapshot/SystemSnapshotProviderTests.cs
+++ Sashiko.SystemMonitor.Tests/Snapshot/SystemSnapshotProviderTests.cs
@@ -1,38 +1,41 @@
 ﻿using Sashiko.SystemMonitor.Snapshot;
 
-public class SystemSnapshotProviderTests
+namespace Sashiko.SystemMonitor.Tests.Snapshot
 {
-	[Fact]
-	public void Capture_ShouldNotThrow()
+	public class SystemSnapshotProviderTests
 	{
-		var exception = Record.Exception(() => SystemSnapshotProvider.Capture());
+		[Fact]
+		public void Capture_ShouldNotThrow()
+		{
+			var exception = Record.Exception(() => SystemSnapshotProvider.Capture());
 
-		Assert.Null(exception);
-	}
+			Assert.Null(exception);
+		}
 
-	[Fact]
-	public void Capture_ShouldReturnValidSnapshot()
-	{
-		var snapshot = SystemSnapshotProvider.Capture();
+		[Fact]
+		public void Capture_ShouldReturnValidSnapshot()
+		{
+			var snapshot = SystemSnapshotProvider.Capture();
 
-		Assert.NotNull(snapshot);
-		Assert.NotNull(snapshot.Os);
-		Assert.NotNull(snapshot.Cpu);
-		Assert.NotNull(snapshot.Gpu);
-		Assert.NotNull(snapshot.Memory);
-		Assert.NotNull(snapshot.Disk);
-		Assert.NotNull(snapshot.Thermal);
-		Assert.NotNull(snapshot.Power);
-	}
+			Assert.NotNull(snapshot);
+			Assert.NotNull(snapshot.Os);
+			Assert.NotNull(snapshot.Cpu);
+			Assert.NotNull(snapshot.Gpu);
+			Assert.NotNull(snapshot.Memory);
+			Assert.NotNull(snapshot.Disk);
+			Assert.NotNull(snapshot.Thermal);
+			Assert.NotNull(snapshot.Power);
+		}
 
-	[Fact]
-	public void Capture_ShouldReturnConsistentStructure()
-	{
-		var s1 = SystemSnapshotProvider.Capture();
-		var s2 = SystemSnapshotProvider.Capture();
+		[Fact]
+		public void Capture_ShouldReturnConsistentStructure()
+		{
+			var s1 = SystemSnapshotProvider.Capture();
+			var s2 = SystemSnapshotProvider.Capture();
 
-		Assert.Equal(s1.GetType(), s2.GetType());
-		Assert.Equal(s1.Cpu.GetType(), s2.Cpu.GetType());
-		Assert.Equal(s1.Memory.GetType(), s2.Memory.GetType());
+			Assert.Equal(s1.GetType(), s2.GetType());
+			Assert.Equal(s1.Cpu.GetType(), s2.Cpu.GetType());
+			Assert.Equal(s1.Memory.GetType(), s2.Memory.GetType());
+		}
 	}
 }
